8aab78ab005a946f98c88740f1a353cb67391da7
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-07-22 14:21:36 -0300

    presentation ready

--- App/scripts/apresentar.js
+++ App/scripts/apresentar.js
@@ -1,51 +1,61 @@
-var button = document.querySelector('#apresentar'),
-    current_slide = 0;
+var next_btns = document.querySelectorAll('.next_btn'),
+    current_slide = 0,
+    stay_at = 0; //Develop VAR
 
-button.addEventListener('click', function(){slide('next')});
-document.addEventListener('keypress', (KeyboardEvent)=>{
-    if(KeyboardEvent.key == "b"){
-        slide('past');
-    }
-    else if(KeyboardEvent.key == "n"){
-        slide('next');
-    }
-})
+if (stay_at > 0) {
+    /*Isso para a apresentação no slide de stay_at
+    para que você desenvolva melhor */
+    let slides = document.querySelectorAll(".sec");
+
+    slides.forEach(slide => {
+        slide.classList.remove('show');
+        slide.style.display = "none";
+    });
+    current_slide = stay_at - 1;
+    slide('next');
+}
+else {
+    next_btns.forEach(btn => {
+        btn.addEventListener('click', function () { slide('next') })
+    });
+    document.addEventListener('keypress', (KeyboardEvent) => {
+        if (KeyboardEvent.key == "b") {
+            slide('past');
+        }
+        else if (KeyboardEvent.key == "n") {
+            slide('next');
+        }
+    })
+}
 
 function slide(option) {
-    // button.classList.add("clicked");
+    // present.classList.add("clicked");
     var past = document.querySelector("#s" + (current_slide - 1)),
         current = document.querySelector("#s" + current_slide),
         next = document.querySelector("#s" + (current_slide + 1));
-    
-        console.log("Antes :"+current_slide);
 
-    if(option == "next"){
+    if (option == "next") {
         current.classList.remove("show");
-        current.classList.add("hide");
-        setTimeout(() => {
-        current.style.display = "none";
-        next.style.display = "block";
-        setTimeout(() => {
-            next.classList.remove("hide")
-            next.classList.add("show");
-        }, 50);
-    }, 250);
-        current_slide++;
-    }
-    else if(option == "past"){
-        current.classList.remove('show');
-        current.classList.add("hide");
         setTimeout(() => {
             current.style.display = "none";
-            past.style.display = "block";
+            next.style.display = "block";
             setTimeout(() => {
-                past.classList.remove("hide");
-                past.classList.add("show");
+                next.classList.add("show");
             }, 50);
-        }, 250);
-        current_slide --;
+        }, 2000);
+        current_slide++;
     }
-
-    console.log("Depois: "+current_slide);
-    
+    else if (option == "past") {
+        if (current_slide != 0) {
+            current.classList.remove('show');
+            setTimeout(() => {
+                current.style.display = "none";
+                past.style.display = "block";
+                setTimeout(() => {
+                    past.classList.add("show");
+                }, 50);
+            }, 2000);
+            current_slide--;
+        }
+    }
 }
